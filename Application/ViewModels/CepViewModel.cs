8ce4c000086e45e8a4f584a98b68a605b06f59f9
Ryan Pablo Silva Araújo <ryan.pablo.silva.araujo@gmail.com>
2023-08-17 00:15:04 -0300

    feature/consulta_cep (#3)
    * Alocando e refatorando dependência do serviço da api de cep
    
    * corrigindo testes unitarios com seus mocks e instâncias do serviço de CEP

---
+++ Application/ViewModels/CepViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.ViewModels
+{
+    public class CepViewModel
+    {
+        public CepViewModel(string cep, string logradouro, string complemento, string bairro, string localidade, string uF)
+        {
+            Cep = cep;
+            Logradouro = logradouro;
+            Complemento = complemento;
+            Bairro = bairro;
+            Localidade = localidade;
+            UF = uF;
+        }
+
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+    }
+}
